22fc61aa2c60c544c299562608b55f7d7e8cb5ef
kf <7kfpun@gmail.com>
2026-08-05 19:35:46 +0800

    Initial Commit

---
+++ nihongoTests/nihongoTests.swift
@@ -0,0 +1,19 @@
+//
+//  nihongoTests.swift
+//  nihongoTests
+//
+//  Created by KF PUN on 5/8/26.
+//
+
+import Testing
+@testable import nihongo
+
+struct nihongoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
